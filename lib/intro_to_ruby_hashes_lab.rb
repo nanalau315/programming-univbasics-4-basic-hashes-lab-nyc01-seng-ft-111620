473980bad9c87b67f5bad5c9c6611cacdf0b9c14
Nana Lau <nanalau315@hotmail.com>
2020-11-05 21:33:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,20 +1,20 @@
 def new_hash
   # return an empty hash
-  new_hash = Hash.new
+  Hash.new
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
-  new_hash = {:fruit => "Apple"}
+  {:fruit => "Apple"}
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
-  new_hash = {:name => "Grace Hopper"}
+  {:name => "Grace Hopper"}
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
-  new_hash = {:id => number}
+  {:id => number}
 end
