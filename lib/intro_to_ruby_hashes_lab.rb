e62a5075c3f878af99f9a42fb648bfe0fbf1ea9a
Nana Lau <nanalau315@hotmail.com>
2020-11-05 21:31:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -5,14 +5,16 @@ end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
-  
+  new_hash = {:fruit => "Apple"}
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  new_hash = {:name => "Grace Hopper"}
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+  new_hash = {:id => number}
 end
